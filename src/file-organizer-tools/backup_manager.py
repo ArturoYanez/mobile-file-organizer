68878432b93739de872098dd9e0287f213c318c9
ArturoYanez <Arturo_w_0@hotmail.com>
2025-02-17 01:12:17 -0400

    feat: add zip backup creator MVP
    - Backup creator Main function added with os.walk and zipfile
    
    Refs #2

--- src/file-organizer-tools/backup_manager.py
+++ src/file-organizer-tools/backup_manager.py
@@ -1,6 +1,16 @@
-categories = {
-        'documents': {
-            'text': ('.txt', '.md', '.rtf')
-            }
-        }
-print(categories)
+import os
+from datetime import datetime
+from zipfile import ZipFile, ZIP_DEFLATED
+
+
+def create_backup(source_dir: str):
+    timestamp = datetime.now().strftime("%d%m%y_%H%M%S")
+    zip_name = f'backup_{timestamp}.zip'
+    with ZipFile(zip_name, 'w', ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(source_dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, source_dir)
+                zipf.write(file_path, arcname)
+
+create_backup(os.getcwd())
